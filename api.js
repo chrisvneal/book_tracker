b3cb5abef3cce9087c83dce7911111c88c32b6be
Chris Neal <chrisvneal@gmail.com>
2025-03-03 11:14:09 -1000

    Create getTitleISBN function to retrieve ISBN from Google Books API

--- api.js
+++ api.js
@@ -20,6 +20,15 @@ const db = new pg.Client({
 
 db.connect();
 
+async function getTitleISBN(title) {
+	const data = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${title}#`);
+
+	const result = data.data.items[0];
+	const isbn = result.volumeInfo.industryIdentifiers[0].identifier;
+
+	return isbn;
+}
+
 app.get("/api/search", async (req, res) => {
 	const { isbn, title } = req.query;
 
@@ -31,20 +40,24 @@ app.get("/api/search", async (req, res) => {
 		openLibraryURL += `?isbn=${isbn}`;
 	}
 
-	// console.log("url: " + openLibraryURL);
-
 	const result = await axios.get(openLibraryURL);
-	// const bookData = result.data;
+
 	const bookData = result.data.docs[0];
 
 	if (!bookData) {
 		res.send("No book found");
 		return;
 	} else {
+		let googleISBN = await getTitleISBN(bookData.title);
+
 		const book = {
 			title: bookData.title,
 			author: bookData.author_name,
-			isbn: isbn ? isbn : null,
+			isbn: isbn ? isbn : googleISBN, // updated to use googleISBN
+			cover_id: bookData.cover_i,
+			cover_url_small: `http://covers.openlibrary.org/b/id/${bookData.cover_i}-S.jpg`,
+			cover_url_medium: `http://covers.openlibrary.org/b/id/${bookData.cover_i}-M.jpg`,
+			cover_url_large: `http://covers.openlibrary.org/b/id/${bookData.cover_i}-L.jpg`,
 		};
 		res.status(200).json(book);
 	}
