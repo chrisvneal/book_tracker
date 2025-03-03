39917a4ed6126eba5f1e8564386c12631fc8a25b
Chris Neal <chrisvneal@gmail.com>
2025-03-02 14:40:22 -1000

    Configure database

---
+++ api.js
@@ -0,0 +1,24 @@
+import express from "express";
+import pg from "pg";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+
+const API_PORT = process.env.API_PORT || 4000;
+const db = new pg.Client({
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT,
+});
+
+db.connect();
+
+app.listen(API_PORT, () => {
+	console.log(`Server is running on port ${API_PORT}`);
+});
