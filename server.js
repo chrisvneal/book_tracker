82299de1267c6391dd5eacaf57236177b5555563
Chris Neal <chrisvneal@gmail.com>
2025-03-11 13:38:22 -1000

    Implement search history logging and refactor book data retrieval in API

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import ejs from "ejs";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import axios from "axios";
@@ -9,27 +8,31 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const MAIN_PORT = process.env.MAIN_PORT || 3000;
-const API_URL = process.env.API_URL;
 app.use(express.static("public"));
 
+const MAIN_PORT = process.env.MAIN_PORT || 3000;
+const API_URL = process.env.API_URL || "http://localhost:4000";
+
+// Render main page
 app.get("/", (req, res) => {
 	res.render("index.ejs");
 });
 
+// Retrieve book data from API
 app.post("/search", async (req, res) => {
-	// const query = req.body.query;
 	const { query, profile_id } = req.body;
-	// console.log(query, profile_id);
 
 	// Check if the query is an ISBN
 	const isISBN = (/^[\d-]+$/.test(query) && query.replace(/-/g, "").length === 10) || query.replace(/-/g, "").length === 13;
 
-	const API_URL = process.env.API_URL || "http://localhost:4000";
-
 	try {
+		// retrieve results from API
 		const results = await axios.post(`${API_URL}/api/search`, isISBN ? { isbn: query } : { query });
 
+		// post search term to "history" table
+		axios.post(`${API_URL}/api/search-history`, { query, profile_id });
+
+		// Render main page with retrieved book data
 		res.status(200).render("index.ejs", { books: results.data });
 	} catch (error) {
 		res.status(500).render("index.ejs", { books: [], error: "An error occurred while searching for the book." });
