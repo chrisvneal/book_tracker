bfb4b1a5047681523c6c2570c675301744aa2442
Chris Neal <chrisvneal@gmail.com>
2025-03-10 23:49:31 -1000

    Add image display to review section; adjust search form margin for better layout

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,14 @@
 function populateReviewHeader(book) {
 	document.querySelector(".review-book-title").textContent = book.querySelector("h3").textContent;
 	document.querySelector(".review-book-author").textContent = book.querySelector(".book-author").textContent;
+	document.querySelector(".review-book-image img").setAttribute("src", book.querySelector("img").getAttribute("src"));
 }
 
 document.querySelector(".book-search-results").addEventListener("click", function (e) {
 	let book = e.target.closest(".book"); // Find the closest book item
 	if (!book) return; // Stop if clicked outside book items
 
-	console.log("Book item clicked:", book);
+	// console.log("Book item clicked:", book);
 
 	let reviewForm = document.querySelector(".review-form");
 
