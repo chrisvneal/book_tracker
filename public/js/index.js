8d0a8be9914fb58f2135ac2fd66a45106cbb99bb
Chris Neal <chrisvneal@gmail.com>
2025-03-14 18:12:53 -1000

    Update review header to use large cover image and include cover URL in book data attributes

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,19 @@
-function getColorURL(cover_id, size) {
-	let url = `http://covers.openlibrary.org/b/id/${cover_id}-${size.toUpperCase()}.jpg`;
-	return url;
-}
-
 function populateReviewHeader(book) {
-	// get review book title, author name, isbn from selected book
-	let title, author, isbn, published_date, cover_id;
+	// get review book title, author name, isbn, larger size from selected book
+	let title, author, isbn, published_date, cover_id, cover_url_large;
 
 	title = book.querySelector("h3").textContent.trim();
 	author = book.querySelector(".book-author span").textContent.trim();
 	isbn = book.querySelector(".book-isbn span").textContent.trim();
 	published_date = book.querySelector(".book-published-date span").textContent.trim();
 	cover_id = book.getAttribute("data-cover-id");
+	cover_url_large = book.getAttribute("data-cover-large");
 
 	document.querySelector(".review-book-title").textContent = title;
 	document.querySelector(".review-book-author span").textContent = author;
 	document.querySelector(".review-book-isbn span").textContent = isbn;
 	document.querySelector(".review-book-published-date span").textContent = published_date;
+	document.querySelector(".review-book-image img").setAttribute("src", "ugly.jpg");
 
 	// Set hidden input values for form submission
 	document.getElementById("review-book-title").value = title;
@@ -25,7 +22,7 @@ function populateReviewHeader(book) {
 	document.getElementById("review-book-published-date").value = published_date;
 	document.getElementById("review-book-id").value = cover_id;
 
-	document.querySelector(".review-book-image img").setAttribute("src", book.querySelector("img").getAttribute("src"));
+	document.querySelector(".review-book-image img").setAttribute("src", cover_url_large);
 	// document.querySelector(".review-book").setAttribute("data-id", book.closest(".book").getAttribute("data-id"));
 }
 
